f7845dbc901226dd922ed5986c234ffb8fd1fb90
MohanKumar21 <mohanmrm20@gmail.com>
2024-07-09 12:22:34 +0530

    Added EAIRL code

---
+++ irl/agents/agent.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import numpy as np
+
+from utils.utils import ReplayBuffer, make_one_mini_batch, convert_to_tensor
+
+class Agent(nn.Module):
+    def __init__(self,algorithm, writer, device, state_dim, action_dim, args, demonstrations_location_args): 
+        super(Agent, self).__init__()
+        self.writer = writer
+        self.device = device
+        self.args = args
+        if self.args.on_policy == True :
+            self.data = ReplayBuffer(action_prob_exist = True, max_size = self.args.traj_length, state_dim = state_dim, num_action = action_dim)
+        else :
+            self.data = ReplayBuffer(action_prob_exist = False, max_size = int(self.args.memory_size), state_dim = state_dim, num_action = action_dim)
+        file_size = 120
+        
+        f = open(demonstrations_location_args.expert_state_location,'rb')
+        self.expert_states = torch.tensor(np.concatenate([np.load(f) for _ in range(file_size)])).float()
+        f = open(demonstrations_location_args.expert_action_location,'rb')
+        self.expert_actions = torch.tensor(np.concatenate([np.load(f) for _ in range(file_size)]))
+        f = open(demonstrations_location_args.expert_next_state_location,'rb')
+        self.expert_next_states = torch.tensor(np.concatenate([np.load(f) for _ in range(file_size)])).float()
+        f = open(demonstrations_location_args.expert_done_location,'rb')
+        self.expert_dones = torch.tensor(np.concatenate([np.load(f) for _ in range(file_size)])).float().unsqueeze(-1)
+        f.close()
+        
+        self.brain = algorithm
+        
+    def get_action(self,x):
+        action, log_prob = self.brain.get_action(x)
+        return action, log_prob
+    
+    def put_data(self,transition):
+        self.data.put_data(transition)
+    
+    def train(self, discriminator, discriminator_batch_size, state_rms, n_epi, batch_size = 64):
+        if self.args.on_policy :
+            data = self.data.sample(shuffle = False)
+            states, actions, rewards, next_states, done_masks, old_log_probs = convert_to_tensor(self.device, data['state'], data['action'], data['reward'], data['next_state'], data['done'], data['log_prob'])
+        else :
+            data = self.data.sample(shuffle = True, batch_size = discriminator_batch_size)
+            states, actions, rewards, next_states, done_masks = convert_to_tensor(self.device, data['state'], data['action'], data['reward'], data['next_state'], data['done'])
+        if discriminator.name() == 'sqil':
+            agent_s,agent_a,agent_next_s,agent_done_mask = make_one_mini_batch(batch_size, states, actions, next_states, done_masks)
+            expert_s,expert_a,expert_next_s,expert_done = make_one_mini_batch(batch_size, self.expert_states, self.expert_actions, self.expert_next_states, self.expert_dones) 
+            expert_done_mask = (1 - expert_done.float())
+
+            discriminator.train_network(self.brain, n_epi, agent_s, agent_a, agent_next_s, agent_done_mask, expert_s, expert_a, expert_next_s, expert_done_mask)
+            return
+        if discriminator.args.is_airl == False:
+            agent_s,agent_a = make_one_mini_batch(discriminator_batch_size, states, actions)
+            expert_s,expert_a = make_one_mini_batch(discriminator_batch_size, self.expert_states, self.expert_actions)
+            if self.args.on_policy :
+                expert_s = np.clip((expert_s - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5)
+            discriminator.train_network(self.writer, n_epi, agent_s, agent_a, expert_s, expert_a)
+        else:
+            agent_s,agent_a,agent_next_s,agent_done_mask = make_one_mini_batch(discriminator_batch_size, states, actions, next_states, done_masks)
+            expert_s,expert_a,expert_next_s,expert_done = make_one_mini_batch(discriminator_batch_size, self.expert_states, self.expert_actions, self.expert_next_states, self.expert_dones) 
+
+            expert_done_mask = (1 - expert_done.float())
+            if self.args.on_policy :
+                expert_s = np.clip((expert_s - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5).float()
+                expert_next_s = np.clip((expert_next_s - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5).float()
+
+            mu,sigma = self.brain.get_dist(agent_s.float().to(self.device))
+            dist = torch.distributions.Normal(mu,sigma)
+            agent_log_prob = dist.log_prob(agent_a).sum(-1,keepdim=True).detach()
+            mu,sigma = self.brain.get_dist(expert_s.float().to(self.device))
+            dist = torch.distributions.Normal(mu,sigma)
+            expert_log_prob = dist.log_prob(expert_a.to(self.device)).sum(-1,keepdim=True).detach()
+            discriminator.train_network(self.writer, n_epi, agent_s, agent_a, agent_next_s,\
+                                          agent_log_prob, agent_done_mask, expert_s, expert_a, expert_next_s, expert_log_prob, expert_done_mask)
+        if self.args.on_policy :
+            self.brain.train_network(self.writer, n_epi, states, actions, rewards, next_states, done_masks, old_log_probs)
+        else : 
+            data = self.data.sample(shuffle = True, batch_size = batch_size)
+            states, actions, rewards, next_states, done_masks = convert_to_tensor(self.device, data['state'], data['action'], data['reward'], data['next_state'], data['done'])
+            self.brain.train_network(self.writer, n_epi, states, actions, rewards, next_states, done_masks)
---
+++ irl/main.py
@@ -0,0 +1,172 @@
+from agents.ppo    import PPO
+from agents.agent            import Agent
+
+
+from discriminators.eairl    import EAIRL
+from utils.utils             import RunningMeanStd, Dict, make_transition
+
+from configparser            import ConfigParser
+from argparse                import ArgumentParser
+
+import os
+import gym
+import numpy as np
+
+import torch
+
+os.makedirs('./model_weights', exist_ok=True)
+
+env = gym.make("Hopper-v4")
+
+action_dim = env.action_space.shape[0]
+state_dim = env.observation_space.shape[0]
+
+parser = ArgumentParser('parameters')
+
+
+parser.add_argument('--test', type=bool, default=False, help="True if test, False if train (default: False)")
+parser.add_argument('--render', type=bool, default=False, help="(default: False)")
+parser.add_argument('--epochs', type=int, default=1001, help='number of epochs, (default: 1001)')
+parser.add_argument("--agent", type=str, default = 'ppo', help = 'actor training algorithm(default: ppo)')
+parser.add_argument("--discriminator", type=str, default = 'eairl', help = 'discriminator training algorithm(default: gail)')
+parser.add_argument("--save_interval", type=int, default = 100, help = 'save interval')
+parser.add_argument("--print_interval", type=int, default = 1, help = 'print interval')
+parser.add_argument('--tensorboard', type=bool, default=True, help='use_tensorboard, (default: True)')
+
+args = parser.parse_args()
+parser = ConfigParser()
+parser.read('config.ini')
+
+demonstrations_location_args = Dict(parser,'demonstrations_location',True)
+agent_args = Dict(parser,args.agent)
+discriminator_args = Dict(parser,args.discriminator)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device='cpu'
+if args.tensorboard:
+    from torch.utils.tensorboard import SummaryWriter
+    writer = SummaryWriter()
+else:
+    writer = None
+
+
+if args.discriminator == 'eairl':
+    discriminator = EAIRL(writer, device, state_dim, action_dim, discriminator_args)
+else:
+    raise NotImplementedError
+    
+if args.agent == 'ppo':
+    algorithm = PPO(device, state_dim, action_dim, agent_args)
+else:
+    raise NotImplementedError
+agent = Agent(algorithm, writer, device, state_dim, action_dim, agent_args, demonstrations_location_args)
+if device == 'cuda':
+    agent = agent.cuda()
+    discriminator = discriminator.cuda()
+    
+state_rms = RunningMeanStd(state_dim)
+
+score_lst = []
+discriminator_score_lst = []
+score = 0.0
+discriminator_score = 0
+if agent_args.on_policy == True:
+    state_lst = []
+    state_,_ = (env.reset())
+    state = np.clip((state_ - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5)
+    for n_epi in range(args.epochs):
+        for t in range(agent_args.traj_length):
+            if args.render:    
+                env.render()
+            state_lst.append(state_)
+            
+            action, log_prob = agent.get_action(torch.from_numpy(state).float().unsqueeze(0).to(device))
+            
+            next_state_, r, done,terminated, info = env.step(action.cpu().numpy()[0])
+            next_state = np.clip((next_state_ - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5)
+            if discriminator_args.is_airl:
+                reward = discriminator.get_reward(\
+                                        log_prob,\
+                                        torch.tensor(state).unsqueeze(0).float().to(device),action,\
+                                        torch.tensor(next_state).unsqueeze(0).float().to(device),\
+                                                              torch.tensor(done).view(1,1)\
+                                                 ).item()
+            else:
+                reward = discriminator.get_reward(torch.tensor(state).unsqueeze(0).float().to(device),action).item()
+
+            transition = make_transition(state,\
+                                         action,\
+                                         np.array([reward/10.0]),\
+                                         next_state,\
+                                         np.array([done]),\
+                                         log_prob.detach().cpu().numpy()\
+                                        )
+            agent.put_data(transition) 
+            score += r
+            discriminator_score += reward
+            if done:
+                state_,_ = (env.reset())
+                state = np.clip((state_ - state_rms.mean) / (state_rms.var ** 0.5 + 1e-8), -5, 5)
+                score_lst.append(score)
+                if writer != None:
+                    writer.add_scalar("score/real", score, n_epi)
+                    writer.add_scalar("score/discriminator", discriminator_score, n_epi)
+                score = 0
+                discriminator_score = 0
+            else:
+                state = next_state
+                state_ = next_state_
+        agent.train(discriminator, discriminator_args.batch_size, state_rms, n_epi)
+        state_rms.update(np.vstack(state_lst))
+        state_lst = []
+        if n_epi%args.print_interval==0 and n_epi!=0:
+            print("# of episode :{}, avg score : {:.1f}".format(n_epi, sum(score_lst)/len(score_lst)))
+            score_lst = []
+        if (n_epi % args.save_interval == 0 )& (n_epi != 0):
+            torch.save(agent.state_dict(), './model_weights/model_'+str(n_epi))
+else : #off-policy
+    for n_epi in range(args.epochs):
+        score = 0.0
+        discriminator_score = 0.0
+        state = env.reset()
+        done = False
+        while not done:
+            if args.render:    
+                env.render()
+            action_, log_prob = agent.get_action(torch.from_numpy(state).float().to(device))
+            action = action_.cpu().detach().numpy()
+            next_state, r, done, info = env.step(action)
+            if discriminator_args.is_airl:
+                reward = discriminator.get_reward(\
+                            log_prob,
+                            torch.tensor(state).unsqueeze(0).float().to(device),action_,\
+                            torch.tensor(next_state).unsqueeze(0).float().to(device),\
+                                                  torch.tensor(done).unsqueeze(0)\
+                                                 ).item()
+            else:
+                reward = discriminator.get_reward(torch.tensor(state).unsqueeze(0).float().to(device),action_).item()
+
+            transition = make_transition(state,\
+                                         action,\
+                                         np.array([reward/10.0]),\
+                                         next_state,\
+                                         np.array([done])\
+                                        )
+            agent.put_data(transition) 
+
+            state = next_state
+
+            score += r
+            discriminator_score += reward
+            
+            if agent.data.data_idx > agent_args.learn_start_size: 
+                agent.train(discriminator, discriminator_args.batch_size, state_rms, n_epi, agent_args.batch_size)
+        score_lst.append(score)
+        if args.tensorboard:
+            writer.add_scalar("score/score", score, n_epi)
+            writer.add_scalar("score/discriminator", discriminator_score, n_epi)
+        if n_epi%args.print_interval==0 and n_epi!=0:
+            print("# of episode :{}, avg score : {:.1f}".format(n_epi, sum(score_lst)/len(score_lst)))
+            score_lst = []
+        if n_epi%args.save_interval==0 and n_epi!=0:
+            torch.save(agent.state_dict(),'./model_weights/agent_'+str(n_epi))
